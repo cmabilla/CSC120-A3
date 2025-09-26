c48c3da4acffcf59d03a9cd3e4cdd17f17f8eeed
cmabilla <cabilla@smith.edu>
2025-09-25 23:52:54 -0400

    Catherine A3 HW

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,100 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.ArrayList;
 
-class Conversation implements ConversationRequirements {
+public class Conversation implements ConversationRequirements {
 
   // Attributes 
+  int number_rounds;
+  String user_response;
+  String [] responses;
+  String [] default_responses;
+  String [] possible_pronouns;
+  int i;
+  int amount;
+  int array_length;
+  String newVariable;
+  ArrayList<String> transcript = new ArrayList <>();
 
   /**
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation() {
+    String [] responses = {};
+    String [] default_responses = {"Mhm","Okay!", "Sounds good", "Oh wow", "Great!"};
+    String [] possible_pronouns = {"i", "me", "am", "you", "my", "your"};
+    this.responses = responses;
+    this.default_responses = default_responses;
+    this.possible_pronouns = possible_pronouns;
+    transcript = new ArrayList <String>();//honestly, I only did this because I couldnt...
+    //..find a way to add strings to the other list type I used above. This didn't work though.
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner input = new Scanner (System.in);//Not sure why its yellow
+    System.out.println ("How many rounds?");
+    transcript.add("How many rounds?");
+    int amount = input.nextInt();
+    input.nextLine();
+    transcript.add(amount+"");
+    System.out.println ("Hi! What's on your mind?");
+    transcript.add("Hi! What's on your mind?");
+    //input.close();
+    for (int i = 0; i <= amount ; i++){//It never does the exact amount of rows
+      String user_response = input.nextLine();
+      transcript.add(user_response);
+      user_response = user_response.toLowerCase();//.equalsIgnoreCase works too
+      String [] responses = user_response.split(" ");//what happens if its not split by spaces?
+      //System.out.println(Arrays.toString(responses));
+      int array_length = responses.length;
+      //System.out.println(array_length);
+      for (int j = 0; j < array_length; j++){
+        for (String key : possible_pronouns){
+          if (responses[j].equals(key)){
+            responses[j] = respond(responses[j]);
+            //System.out.println(Arrays.toString(responses));
+            break;//This prevents a repetition or replacing already swapped words
+          }
+        }
+      }
+      String new_responses = String.join(" ",responses);
+      if (new_responses.equalsIgnoreCase(user_response)){
+        Random random = new Random();
+        
+        String newVariable = (default_responses[random.nextInt(default_responses.length)]);
+        transcript.add(newVariable);//Doesn't work
 
-  }
+      }else{
+        System.out.println(new_responses);
+        transcript.add(new_responses);//this should ideally work but it doesn't
+      }
+    }
+}
+  //THIS WAS AN INITIAL ATTEMPT AT READING MY USER RESPONSE FOR MIRROR WORD
+  //     for (String word:responses){
+  //       for (String key : possible_pronouns){
+  //         if (word.equalsIgnoreCase(key)){
+  //           respond(word);
+  //         } else {
+
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    for (String i : transcript){
+      System.out.println(i);//This entire method is not working 
+      //I did an ArrayList instead of whatever I was doing before 
+    }
   }
 
   /**
@@ -30,6 +103,18 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
+
+    if (inputString.equals("i")|inputString.equals("me")){
+      return "you"; 
+    } else if (inputString.equals("you")){
+      return "I";
+    } else if (inputString.equals("am")){
+      return "are";
+    } else if (inputString.equals("my")){
+      return "your";
+    } else if (inputString.equals("your")){
+      return "my";
+    }
     String returnString = ""; 
     return returnString; 
   }
@@ -39,6 +124,7 @@ class Conversation implements ConversationRequirements {
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
+    //my printTranscript does not work and I am not sure why
 
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
+- Which classmates did you consult with while working on this assignment? 
+Kendall, Elsie, Isa, Kayla, Aris
+
 - Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
+ 5 hours total (4 TA hours, 1 Office Hour) and counting...
+
+- What are your initial impressions of Java?
+ I do not like it. It has way too specific syntax and too many functions for such simple things. It is very annoying.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+They share the same operators, and the.... Java has a way more specific syntax, as well as more functions which you need to know how to use to be successful (Scanner, ArrayList, printing etc.). I find that python is easier to look at due to the lack of curly brackets, however with Java the indentation of some things doesn't matter. Java also seems to be a complier whereas Python may be an interpreted language (I could be wrong though). In general, maybe because I am new to Java, I feel like you have to be a lot more aware when coding it, because the slightest of things, like punctuation or functions missing will mess up the code. Java does have a few cool functions like .equalsIgnoreCase.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+What worked for me was mapping out my work on a piece of paper going step by step. Google was my best friend for finding functions to be used (Oracle, w3schools & Stack Overflow). Please try and use one type of array lists, I have not clue what I did and now my printing does not work. Do not let people claiming that they are 'done' with the assignment let you feel like your behind, all work will eventually work and we all have different ways of getting there. I still want to try the Kudos stuff, but try ask a Professor or TA for those, since my peers found it a bit hard too. 
